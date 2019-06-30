58e44426cdc807fe8d16b07b45eb37d795d6d63e
luoquan <nchkdxlq.gmail.com>
2019-06-30 16:43:30 +0800

    set & priority_queue

--- STLDemo/STLDemo/priority_queue/priorityQueue.cpp
+++ STLDemo/STLDemo/priority_queue/priorityQueue.cpp
@@ -11,8 +11,52 @@
 #include <vector>
 #include <iostream>
 
-
 using namespace std;
+
+
+/**
+ https://zh.cppreference.com/w/cpp/container/priority_queue
+ 
+ class T,
+ class Container = std::vector<T>,
+ class Compare = std::less<typename Container::value_type>
+ > class priority_queue;
+ 
+ ## 时间复杂度
+ 查找最值: queue.top(): O(1)
+ 插入元素: queue.push(1): O( log(n) )
+ 删除堆顶元素: queue.pop() O( log(n) )
+ 
+ 
+ priority_queue默认情况下是大顶堆，top是priority_queue中最大的值。
+ 
+ 如果想要实现小顶堆，需要在初始化的时候指定比较函数。
+ 
+ 
+ ## 常用方法
+ 
+ 访问元素
+ q.top(); //访问堆顶元素
+ 
+ 容量
+ q.size();
+ q.empty();
+ 
+ 修改器
+ q.push(1); // 插入元素，并对底层容器排序
+ q.pop(); // 删除堆顶元素
+ 
+ */
+
+// 自定义比较函数
+
+struct MyCompare {
+    bool operator()(const int &l, int &r) const
+    {
+        return l > r;
+    }
+};
+
 
 
 template<typename T> void print_queue(T& q) {
@@ -38,7 +82,7 @@ void priority_queue_entry() {
     
     
     // 创建 小顶堆
-    priority_queue<int, vector<int>, greater<int>> q2;
+    priority_queue<int, vector<int>, MyCompare> q2;
     for (auto i : iv) {
         q2.push(i);
     }
--- STLDemo/STLDemo/set/set.cpp
+++ STLDemo/STLDemo/set/set.cpp
@@ -14,6 +14,51 @@
 using namespace std;
 
 
+
+/**
+ https://zh.cppreference.com/w/cpp/container/set
+ 
+ template<
+ class Key,
+ class Compare = std::less<Key>,
+ class Allocator = std::allocator<Key>
+ > class set;
+ 
+ set时间时间复杂度分析
+ 1. 插入：nlog(n)
+ 2. 删除：nlog(n)
+ 3. 查找：nlog(n)
+ 
+ 底层实现：红黑树
+ 
+ 容量
+ s.empty();
+ s.size();
+ s.max_size();
+ 
+ 
+ 修改
+ s.insert(1); // 插入一个元素
+ s.erase(2); // 删除指定的值
+ s.clear(); // 清空set
+ 
+ 
+ 查找
+ s.count(1); // 返回指定元素的个数，1：存在， 0：不存在
+ s.find(1); // 返回指定元素的iterator， 如果不存在 返回s.end()
+ 
+ 
+ 
+ 迭代器
+ s.begin(); // 指向第一个元素
+ s.end(); // 指向最后一个元素的后一个位置
+ 
+ s.rbegin(); // 指向最后一个元素
+ s.rend(); // 指向第一个元素前一个位置
+ 
+ */
+
+
 int thirdMax(vector<int>& nums) {
     set<int> s;
     
@@ -55,24 +100,40 @@ void set_entry() {
     s.insert(2);
     s.insert(1);
     
-    for (auto &num : s) {
+    for (auto num : s) {
         cout << num << " ";
     }
     cout << endl;
     
+    cout << "======== 顺序遍历 ========" << endl;
     for (auto i = s.begin(); i != s.end(); i++) {
         cout << *i << " ";
     }
     cout << endl;
     
+    cout << "======== 逆序遍历 ========" << endl;
+    for (auto i = s.rbegin(); i != s.rend(); i++) {
+        cout << *i << " ";
+    }
+    cout << endl;
+    
+    
     cout << "size = " << s.size() << endl;
     
+    // iterator 是一个指针, 例如 s.begin(), s.end()
     cout << "first  = " << *(s.begin()) << endl;
     cout << "last  = " << *(s.rbegin()) << endl;
     
+    // 查找set中指定元素的个数
     int target = 1;
     auto count1 = s.count(target);
     cout << target << ((count1 == 1) ? " is" : " is not") << " in set" << endl;
+    
+    // 查找set中是否存在某个元素, 返回这个元素的iterator
+    auto find = s.find(target);
+    cout << target << (find != s.end() ? " is" : " is not") << " in set" << endl;
+    
+    
     // 清除某个元素
     s.erase(1);
     cout << "erase " << target << endl;
